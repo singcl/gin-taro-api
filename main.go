8debcce8374d7fe39215ed153193e84240a33ece
singcl <iambabyer@gamil.com>
2022-06-07 00:38:38 +0800

    feat(http):asciiJSON

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -8,6 +12,14 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	// https://gin-gonic.com/zh-cn/docs/examples/ascii-json/
+	r.GET("/asciiJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<button/>",
+		}
+		c.AsciiJSON(http.StatusOK, data)
 	})
 	r.Run() // // 监听并在 0.0.0.0:8080 上启动服务
 }
