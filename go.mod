a93f0e57dfab2a1720840c6a522b6f45b2ebe3cb
singcl <1276837689@qq.com>
2022-06-07 15:05:47 +0800

    feat(api): update main.go

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
@@ -17,7 +17,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
-	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,18 +17,6 @@ func main() {
 		})
 	})
 
-	r.GET("/", func(c *gin.Context) {
-		if pusher := c.Writer.Pusher(); pusher != nil {
-			// 使用 pusher.Push() 做服务器推送
-			if err := pusher.Push("/assets/app.js", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
-		}
-		c.HTML(http.StatusOK, "https.tmpl", gin.H{
-			"status": "success",
-		})
-	})
-
 	// https://gin-gonic.com/zh-cn/docs/examples/ascii-json/
 	r.GET("/asciiJSON", func(c *gin.Context) {
 		data := map[string]interface{}{
@@ -39,15 +26,6 @@ func main() {
 		c.AsciiJSON(http.StatusOK, data)
 	})
 
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-		})
-	})
-
 	// 监听并在 0.0.0.0:9000 上启动服务
 	r.Run(":9000")
-	// 监听并在 https://127.0.0.1:8080 上启动服务
-	// r.RunTLS(":8080", "./testdata/server.pem", "./testdata/server.key")
 }
