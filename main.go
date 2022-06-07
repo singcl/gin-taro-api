ed99c400f12ca91d114b21291ca82f0ae355925f
singcl <1276837689@qq.com>
2022-06-07 12:43:57 +0800

    feat(http): pusher

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+  <script src="/assets/app.js"></script>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, Ginner!</h1>
+</body>
+</html>
+`))
+
 func main() {
 	r := gin.Default()
+	r.Static("/assets", "./assets")
+	r.SetHTMLTemplate(html)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			// 使用 pusher.Push() 做服务器推送
+			if err := pusher.Push("/assets/app.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		c.HTML(http.StatusOK, "https", gin.H{
+			"status": "success",
+		})
+	})
+
 	// https://gin-gonic.com/zh-cn/docs/examples/ascii-json/
 	r.GET("/asciiJSON", func(c *gin.Context) {
 		data := map[string]interface{}{
@@ -30,6 +59,6 @@ func main() {
 		})
 	})
 
-	// 监听并在 0.0.0.0:8080 上启动服务
-	r.Run()
+	// 监听并在 0.0.0.0:9000 上启动服务
+	r.Run(":9000")
 }
