1ac9e5030b0db624052a67cd9987c5f513918571
singcl <1276837689@qq.com>
2022-06-08 18:21:13 +0800

    feat(const): add new const filed

--- configs/constants.go
+++ configs/constants.go
@@ -1,6 +1,12 @@
 package configs
 
 const (
+	// ProjectName 项目名称
+	ProjectName = "gin-taro-api"
+
 	// ProjectPort 项目端口
 	ProjectPort = ":9000"
+
+	// RedisKeyPrefixLoginUser Redis Key 前缀 - 登录用户信息
+	RedisKeyPrefixLoginUser = ProjectName + ":login-user:"
 )
