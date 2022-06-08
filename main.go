883229aacd669024274e22ed2ed6ccd5897312f9
singcl <1276837689@qq.com>
2022-06-08 23:20:28 +0800

    feat: upgrade main code

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/singcl/gin-taro-api/configs"
-	"github.com/singcl/gin-taro-api/routers"
+	"github.com/singcl/gin-taro-api/internal/router"
 )
 
 func main() {
-	r := routers.InitRouter()
-	err := r.Run(configs.ProjectPort)
+	// r := routers.InitRouter()
+	// err := r.Run(configs.ProjectPort)
+	// if err != nil {
+	// 	fmt.Println("服务器启动失败！")
+	// }
+
+	// 初始化 HTTP 服务
+	s, err := router.NewHTTPServer()
 	if err != nil {
-		fmt.Println("服务器启动失败！")
+		panic(err)
 	}
+
+	server := &http.Server{
+		Addr:    configs.ProjectPort,
+		Handler: s.Kiko,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			// logger
+		}
+	}()
 }
