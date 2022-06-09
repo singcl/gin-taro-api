dc98183d3dba24a7064e25fad127766ade4659c9
singcl <1276837689@qq.com>
2022-06-09 15:31:31 +0800

    feat(core): update Kiko context

--- internal/pkg/core/context.go
+++ internal/pkg/core/context.go
@@ -2,20 +2,27 @@ package core
 
 import (
 	"bytes"
+	stdctx "context"
 	"io/ioutil"
 	"net/http"
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/singcl/gin-taro-api/pkg/trace"
+	"go.uber.org/zap"
 )
 
 const (
 	_BodyName       = "_body_"
 	_PayloadName    = "_payload_"
 	_AbortErrorName = "_abort_error_"
+	_LoggerName     = "_logger_"
+	_TraceName      = "_trace_"
 )
 
 type HandlerFunc func(c Context)
+type Trace = trace.T
 
 var _ Context = (*context)(nil)
 
@@ -32,10 +39,28 @@ type Context interface {
 	ShouldBindURI(obj interface{}) error
 	// AbortWithError 错误返回
 	AbortWithError(err BusinessError)
+	// ShouldBindForm 同时反序列化 querystring 和 postform;
+	// 当 querystring 和 postform 存在相同字段时，postform 优先使用。
+	// tag: `form:"xxx"`
+	ShouldBindForm(obj interface{}) error
+	// RequestContext 获取请求的 context (当 client 关闭后，会自动 canceled)
+	RequestContext() StdContext
+
+	// Trace 获取 Trace 对象
+	Trace() Trace
+
+	// Logger 获取 Logger 对象
+	Logger() *zap.Logger
 }
 
 type context struct {
 	ctx *gin.Context
+}
+
+type StdContext struct {
+	stdctx.Context
+	Trace
+	*zap.Logger
 }
 
 /******************* context实现Context接口*******************/
@@ -79,6 +104,41 @@ func (c *context) AbortWithError(err BusinessError) {
 
 		c.ctx.AbortWithStatus(httpCode)
 		c.ctx.Set(_AbortErrorName, err)
+	}
+}
+
+// ShouldBindForm 同时反序列化querystring和postform;
+// 当querystring和postform存在相同字段时，postform优先使用。
+// tag: `form:"xxx"`
+func (c *context) ShouldBindForm(obj interface{}) error {
+	return c.ctx.ShouldBindWith(obj, binding.Form)
+}
+
+func (c *context) Trace() Trace {
+	t, ok := c.ctx.Get(_TraceName)
+	if !ok || t == nil {
+		return nil
+	}
+
+	return t.(Trace)
+}
+
+func (c *context) Logger() *zap.Logger {
+	logger, ok := c.ctx.Get(_LoggerName)
+	if !ok {
+		return nil
+	}
+
+	return logger.(*zap.Logger)
+}
+
+// RequestContext (包装 Trace + Logger) 获取请求的 context (当client关闭后，会自动canceled)
+func (c *context) RequestContext() StdContext {
+	return StdContext{
+		//c.ctx.Request.Context(),
+		stdctx.Background(),
+		c.Trace(),
+		c.Logger(),
 	}
 }
 
