2c1b5b664aca81ba60af9d2fa49617d02ec6d1c9
singcl <1276837689@qq.com>
2022-06-09 15:33:45 +0800

    feat(redis): add redis repositories

--- internal/repository/redis/redis.go
+++ internal/repository/redis/redis.go
@@ -1,3 +1,108 @@
 package redis
 
-type Repo interface{}
+import (
+	"time"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/singcl/gin-taro-api/pkg/errors"
+	"github.com/singcl/gin-taro-api/pkg/timeutil"
+	"github.com/singcl/gin-taro-api/pkg/trace"
+)
+
+type Option func(*option)
+type Trace = trace.T
+
+var _ Repo = (*cacheRepo)(nil)
+
+type option struct {
+	Trace *trace.Trace
+	Redis *trace.Redis
+}
+
+func newOption() *option {
+	return &option{}
+}
+
+type Repo interface {
+	i()
+	Exists(keys ...string) bool
+	Get(key string, options ...Option) (string, error)
+	Set(key, value string, ttl time.Duration, options ...Option) error
+}
+
+type cacheRepo struct {
+	client *redis.Client
+}
+
+func (c *cacheRepo) i() {}
+
+func (c *cacheRepo) Exists(keys ...string) bool {
+	if len(keys) == 0 {
+		return true
+	}
+	value, _ := c.client.Exists(keys...).Result()
+	return value > 0
+}
+
+// Set set some <key,value> into redis
+func (c *cacheRepo) Set(key, value string, ttl time.Duration, options ...Option) error {
+	ts := time.Now()
+	opt := newOption()
+	defer func() {
+		if opt.Trace != nil {
+			opt.Redis.Timestamp = timeutil.CSTLayoutString()
+			opt.Redis.Handle = "set"
+			opt.Redis.Key = key
+			opt.Redis.Value = value
+			opt.Redis.TTL = ttl.Minutes()
+			opt.Redis.CostSeconds = time.Since(ts).Seconds()
+			opt.Trace.AppendRedis(opt.Redis)
+		}
+	}()
+
+	for _, f := range options {
+		f(opt)
+	}
+
+	if err := c.client.Set(key, value, ttl).Err(); err != nil {
+		return errors.Wrapf(err, "redis set key: %s err", key)
+	}
+
+	return nil
+}
+
+// WithTrace 设置trace信息
+func WithTrace(t Trace) Option {
+	return func(opt *option) {
+		if t != nil {
+			opt.Trace = t.(*trace.Trace)
+			opt.Redis = new(trace.Redis)
+		}
+	}
+}
+
+// Get get some key from redis
+func (c *cacheRepo) Get(key string, options ...Option) (string, error) {
+	ts := time.Now()
+	opt := newOption()
+	defer func() {
+		if opt.Trace != nil {
+			opt.Redis.Timestamp = timeutil.CSTLayoutString()
+			opt.Redis.Handle = "get"
+			opt.Redis.Key = key
+			opt.Redis.CostSeconds = time.Since(ts).Seconds()
+			opt.Trace.AppendRedis(opt.Redis)
+		}
+	}()
+
+	for _, f := range options {
+		f(opt)
+	}
+
+	value, err := c.client.Get(key).Result()
+	if err != nil {
+		return "", errors.Wrapf(err, "redis get key: %s err", key)
+	}
+
+	return value, nil
+}
