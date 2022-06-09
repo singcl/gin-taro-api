eb74e6ed09487b71167dcc556d5d8e7bc7b6ffcc
singcl <1276837689@qq.com>
2022-06-09 15:33:24 +0800

    feat(mysql): add mysql repositories

--- internal/repository/mysql/mysql.go
+++ internal/repository/mysql/mysql.go
@@ -1,3 +1,38 @@
 package mysql
 
-type Repo interface{}
+import "gorm.io/gorm"
+
+// Predicate is a string that acts as a condition in the where clause
+type Predicate string
+
+var (
+	EqualPredicate              = Predicate("=")
+	NotEqualPredicate           = Predicate("<>")
+	GreaterThanPredicate        = Predicate(">")
+	GreaterThanOrEqualPredicate = Predicate(">=")
+	SmallerThanPredicate        = Predicate("<")
+	SmallerThanOrEqualPredicate = Predicate("<=")
+	LikePredicate               = Predicate("LIKE")
+)
+
+var _ Repo = (*dbRepo)(nil)
+
+type Repo interface {
+	i()
+	GetDbR() *gorm.DB
+	GetDbW() *gorm.DB
+}
+
+type dbRepo struct {
+	DbR *gorm.DB
+	DbW *gorm.DB
+}
+
+func (d *dbRepo) i() {}
+func (d *dbRepo) GetDbR() *gorm.DB {
+	return d.DbR
+}
+
+func (d *dbRepo) GetDbW() *gorm.DB {
+	return d.DbW
+}
