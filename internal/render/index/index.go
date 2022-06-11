3d8ef53b152aa8242680c3d320bc9b59b0b7b473
singcl <1276837689@qq.com>
2022-06-11 12:20:59 +0800

    feat(render): render fed html

---
+++ internal/render/index/index.go
@@ -0,0 +1,28 @@
+package index
+
+import (
+	"github.com/singcl/gin-taro-api/internal/pkg/core"
+	"github.com/singcl/gin-taro-api/internal/repository/mysql"
+	"github.com/singcl/gin-taro-api/internal/repository/redis"
+	"go.uber.org/zap"
+)
+
+type handler struct {
+	logger *zap.Logger
+	cache  redis.Repo
+	db     mysql.Repo
+}
+
+func New(logger *zap.Logger, db mysql.Repo, cache redis.Repo) *handler {
+	return &handler{
+		logger: logger,
+		cache:  cache,
+		db:     db,
+	}
+}
+
+func (h *handler) Index() core.HandlerFunc {
+	return func(ctx core.Context) {
+		ctx.HTML("index", nil)
+	}
+}
--- internal/router/router_render.go
+++ internal/router/router_render.go
@@ -2,15 +2,20 @@ package router
 
 import (
 	"github.com/singcl/gin-taro-api/internal/pkg/core"
+	"github.com/singcl/gin-taro-api/internal/render/index"
 	"github.com/singcl/gin-taro-api/internal/render/install"
 )
 
 func setRenderRouter(r *resource) {
 	renderInstall := install.New(r.logger)
+	renderIndex := index.New(r.logger, r.db, r.cache)
 
 	// 无需记录日志，无需 RBAC 权限验证
 	notRBAC := r.kiko.Group("", core.DisableTraceLog, core.DisableRecordMetrics)
 	{
+		// 首页
+		notRBAC.GET("", renderIndex.Index())
+
 		// 安装
 		notRBAC.GET("install", renderInstall.View())
 	}
