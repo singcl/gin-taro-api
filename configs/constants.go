17c4a938e02d155bc879388b3e08f1fe1f70b85d
singcl <1276837689@qq.com>
2022-06-11 13:58:36 +0800

    feat(config): const about error

--- configs/constants.go
+++ configs/constants.go
@@ -27,6 +27,15 @@ const (
 	// HeaderSignTokenTimeout 签名有效期为 2 分钟
 	HeaderSignTokenTimeout = time.Minute * 2
 
+	// HeaderLoginToken 登录验证 Token，Header 中传递的参数
+	HeaderLoginToken = "Token"
+
+	// HeaderSignToken 签名验证 Authorization，Header 中传递的参数
+	HeaderSignToken = "Authorization"
+
+	// HeaderSignTokenDate 签名验证 Date，Header 中传递的参数
+	HeaderSignTokenDate = "Authorization-Date"
+
 	// RedisKeyPrefixLoginUser Redis Key 前缀 - 登录用户信息
 	RedisKeyPrefixLoginUser = ProjectName + ":login-user:"
 
