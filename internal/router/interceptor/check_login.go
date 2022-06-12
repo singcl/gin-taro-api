5efa9152aeea847f5acdf43621472a8375a74cc0
singcl <1276837689@qq.com>
2022-06-12 10:26:00 +0800

    feat(router): login interceptor

---
+++ internal/router/interceptor/check_login.go
@@ -0,0 +1,56 @@
+package interceptor
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/singcl/gin-taro-api/configs"
+	"github.com/singcl/gin-taro-api/internal/code"
+	"github.com/singcl/gin-taro-api/internal/pkg/core"
+	"github.com/singcl/gin-taro-api/internal/proposal"
+	"github.com/singcl/gin-taro-api/internal/repository/redis"
+	"github.com/singcl/gin-taro-api/pkg/errors"
+)
+
+func (i *interceptor) CheckLogin(ctx core.Context) (sessionUserInfo proposal.SessionUserInfo, err core.BusinessError) {
+	token := ctx.GetHeader(configs.HeaderLoginToken)
+	if token == "" {
+		err = core.Error(
+			http.StatusUnauthorized,
+			code.AuthorizationError,
+			code.Text(code.AuthorizationError)).WithError(errors.New("Header 中缺少 Token 参数"))
+
+		return
+	}
+
+	if !i.cache.Exists(configs.RedisKeyPrefixLoginUser + token) {
+		err = core.Error(
+			http.StatusUnauthorized,
+			code.AuthorizationError,
+			code.Text(code.AuthorizationError)).WithError(errors.New("请先登录"))
+
+		return
+	}
+
+	cacheData, cacheErr := i.cache.Get(configs.RedisKeyPrefixLoginUser+token, redis.WithTrace(ctx.Trace()))
+	if cacheErr != nil {
+		err = core.Error(
+			http.StatusUnauthorized,
+			code.AuthorizationError,
+			code.Text(code.AuthorizationError)).WithError(cacheErr)
+
+		return
+	}
+
+	jsonErr := json.Unmarshal([]byte(cacheData), &sessionUserInfo)
+	if jsonErr != nil {
+		core.Error(
+			http.StatusUnauthorized,
+			code.AuthorizationError,
+			code.Text(code.AuthorizationError)).WithError(jsonErr)
+
+		return
+	}
+
+	return
+}
