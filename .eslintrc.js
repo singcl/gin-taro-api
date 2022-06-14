3a4ff6e887b0e370e26c00c6c751ae523e87f4c7
singcl <1276837689@qq.com>
2022-06-14 14:37:03 +0800

    perf(eslint): add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    // "no-unused-vars": [2, {"vars": "all", "args": "after-used"}]
+    // 'no-unused-vars': 0,
+  },
+};
