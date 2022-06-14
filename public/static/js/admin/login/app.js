31339c6fd04cc02bff2c2aa46594a9a905729efd
singcl <1276837689@qq.com>
2022-06-15 00:33:20 +0800

    feat: login page complete

--- public/static/js/admin/login/app.js
+++ public/static/js/admin/login/app.js
@@ -1,58 +1,59 @@
-import { h, ref } from "/public/static/js/vue/vue3.esm-browser.js";
-import Md5Con from "/public/static/js/lib/authorization/md5.min.js";
+import { h, ref } from '/public/static/js/vue/vue3.esm-browser.js';
 // 绝对路径导入无法识别类型。 不知道怎么配置？
-import Kiko from "./../../utils/kiko/Kiko.js";
-
-const businessKey = "admin";
-const businessSecret = "12878dd962115106db6d";
+import Kiko from './../../utils/kiko/Kiko.js';
 
 // App
 export default {
   setup() {
     // 账号
-    const username = ref("admin");
+    const username = ref('admin');
     function onInputChange(v) {
       username.value = v;
     }
     // 密码
-    const password = ref("123456");
-    const onPasswordChange = (v) => (password.value = v);
-
+    const password = ref('123456');
     //
-    function handleSubmitClick() {
-      console.log("---", Md5Con.md5());
-      new Kiko().fetch("/api/login", {
-        method: "POST",
-        headers: {
-          // todo
+    const onPasswordChange = (v) => (password.value = v);
+    //
+    async function handleSubmitClick() {
+      const response = await new Kiko().fetch('/api/login', {
+        method: 'POST',
+        body: {
+          username: username.value,
+          password: password.value,
         },
       });
+      const body = await response.json();
+      const token = body && body.token;
+      token && localStorage.setItem(Kiko.getTokenName(), token);
+      //
+      location.href = '/'
     }
 
     // 渲染函数
     return () =>
-      h("div", { class: "admin-login" }, [
-        h("div", { class: "admin-login-form" }, [
-          h("div", { class: "admin-login-form__item" }, [
-            h("label", { for: "username" }, ["用户名:"]),
-            h("input", {
-              name: "username",
-              type: "text",
+      h('div', { class: 'admin-login' }, [
+        h('div', { class: 'admin-login-form' }, [
+          h('div', { class: 'admin-login-form__item' }, [
+            h('label', { for: 'username' }, ['用户名:']),
+            h('input', {
+              name: 'username',
+              type: 'text',
               value: username.value,
-              placeholder: "请输入用户名",
+              placeholder: '请输入用户名',
               onInput: (e) => {
                 // username.value = e.target.value 这样写无法响应式更新，为啥？
                 onInputChange(e.target.value);
               },
             }),
           ]),
-          h("div", { class: "admin-login-form__item" }, [
-            h("label", { for: "password" }, ["密码:"]),
-            h("input", {
-              name: "password",
-              type: "password",
+          h('div', { class: 'admin-login-form__item' }, [
+            h('label', { for: 'password' }, ['密码:']),
+            h('input', {
+              name: 'password',
+              type: 'password',
               value: password.value,
-              placeholder: "请输入密码",
+              placeholder: '请输入密码',
               onInput: (e) => {
                 // password.value = e.target.value 这样写无法响应式更新，为啥？
                 onPasswordChange(e.target.value);
@@ -60,12 +61,12 @@ export default {
             }),
           ]),
 
-          h("div", { class: "admin-login-form__item" }, [
-            h("label"),
+          h('div', { class: 'admin-login-form__item' }, [
+            h('label'),
             h(
-              "button",
-              { class: "login-btn", onClick: handleSubmitClick },
-              "登录"
+              'button',
+              { class: 'login-btn', onClick: handleSubmitClick },
+              '登录'
             ),
           ]),
         ]),
