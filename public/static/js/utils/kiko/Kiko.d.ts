49c6fb172988b604fb4966777d5494acda2825ee
singcl <1276837689@qq.com>
2022-06-14 14:40:31 +0800

    refactor(kiko):temp sumbmit

--- public/static/js/utils/kiko/Kiko.d.ts
+++ public/static/js/utils/kiko/Kiko.d.ts
@@ -1,3 +1,4 @@
+
 export = Kiko;
 
 declare class Kiko {
--- public/static/js/utils/kiko/Kiko.js
+++ public/static/js/utils/kiko/Kiko.js
@@ -2,7 +2,24 @@ export default class Kiko {
   constructor() {
     //
   }
+  /**
+   *
+   * @param {RequestInfo} input
+   * @param {RequestInit} init
+   * @returns
+   */
   fetch(input, init) {
-    return fetch(input);
+    /**
+     * @type {RequestInit}
+     */
+    const authInit = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        Authorization: authorizationData.authorization,
+        'Authorization-Date': authorizationData.date,
+        Token: $.cookie('_login_token_'),
+      },
+    };
+    return fetch(input, Object.assign(authInit, init));
   }
 }
