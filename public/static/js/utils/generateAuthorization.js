c226721f7a5e003ac40db2dd13ad565a83ba62d4
singcl <1276837689@qq.com>
2022-06-15 00:34:31 +0800

    feat(lib): lib umd in browser

---
+++ public/static/js/utils/generateAuthorization.js
@@ -0,0 +1,34 @@
+/* global ksort CryptoJS */
+import jqParam from './jqParam.js';
+
+export default function generateAuthorization({
+  businessKey,
+  businessSecret,
+  path,
+  method,
+  params,
+}) {
+  let date = new Date();
+  let datetime =
+    date.getFullYear() +
+    '-' + // "年"
+    (date.getMonth() + 1 >= 10
+      ? date.getMonth() + 1
+      : '0' + (date.getMonth() + 1)) +
+    '-' + // "月"
+    (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) +
+    ' ' + // "日"
+    (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) +
+    ':' + // "小时"
+    (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) +
+    ':' + // "分钟"
+    (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()); // "秒"
+
+  let sortParamsEncode = decodeURIComponent(jqParam(ksort(params)));
+  let encryptStr =
+    path + '|' + method.toUpperCase() + '|' + sortParamsEncode + '|' + datetime;
+  let digest = CryptoJS.enc.Base64.stringify(
+    CryptoJS.HmacSHA256(encryptStr, businessSecret)
+  );
+  return { authorization: businessKey + ' ' + digest, date: datetime };
+}
