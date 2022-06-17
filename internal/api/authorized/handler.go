dbe7aee6bb85bfa295d382cb7f1c3c64a9f8f0d9
singcl <1276837689@qq.com>
2022-06-18 00:19:00 +0800

    feat(router): authorized create handler

---
+++ internal/api/authorized/handler.go
@@ -0,0 +1,45 @@
+package authorized
+
+import (
+	"github.com/singcl/gin-taro-api/configs"
+	"github.com/singcl/gin-taro-api/internal/pkg/core"
+	"github.com/singcl/gin-taro-api/internal/repository/mysql"
+	"github.com/singcl/gin-taro-api/internal/repository/redis"
+	"github.com/singcl/gin-taro-api/internal/services/authorized"
+	"github.com/singcl/gin-taro-api/pkg/hash"
+	"go.uber.org/zap"
+)
+
+var _ Handler = (*handler)(nil)
+
+type Handler interface {
+	i()
+
+	// Create 新增调用方
+	// @Tags API.authorized
+	// @Router /api/authorized [post]
+	Create() core.HandlerFunc
+
+	// List 调用方列表
+	// @Tags API.authorized
+	// @Router /api/authorized [get]
+	List() core.HandlerFunc
+}
+
+type handler struct {
+	logger            *zap.Logger
+	cache             redis.Repo
+	hashids           hash.Hash
+	authorizedService authorized.Service
+}
+
+func New(logger *zap.Logger, db mysql.Repo, cache redis.Repo) Handler {
+	return &handler{
+		logger:            logger,
+		cache:             cache,
+		authorizedService: authorized.New(db, cache),
+		hashids:           hash.New(configs.Get().HashIds.Secret, configs.Get().HashIds.Length),
+	}
+}
+
+func (h *handler) i() {}
