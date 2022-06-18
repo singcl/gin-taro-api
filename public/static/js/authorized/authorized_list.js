72f32c4f37d2cb33d5a3908fcae103c113201656
singcl <1276837689@qq.com>
2022-06-18 13:35:50 +0800

    feat(components): vue3 components in browser

--- public/static/js/authorized/authorized_list.js
+++ public/static/js/authorized/authorized_list.js
@@ -3,6 +3,9 @@ import naive from '/public/static/js/vue/naive.js';
 // 绝对路径导入无法识别类型。 不知道怎么配置？
 import Kiko from './../utils/kiko/Kiko.js';
 
+//
+import AddDialog from './components/authorized_add.js';
+
 const useOptions = [
   {
     value: 1,
@@ -71,18 +74,27 @@ export default {
   setup() {
     const columns = reactive(defaultColumns);
     const tableData = ref([]);
+    const adModalVisible = ref(false);
     //
     onMounted(async () => {
       const response = await new Kiko().fetch('/api/authorized');
       tableData.value = response.list;
     });
     //
+    function handleAddAuth() {
+      adModalVisible.value = true;
+    }
+    //
+    function handleAddModalCancel(v) {
+      adModalVisible.value = v;
+    }
+    //
     return () =>
       h('div', { style: { maxWidth: '1200px', margin: '0 auto' } }, [
         h(naive.NAlert, { type: 'success', title: '授权信息', style: 'margin-bottom: 10px' }, '已授权信息表格'),
         h('div', [
           h('div', { style: { marginBottom: '10px', textAlign: 'right', padding: '0 12px' } }, [
-            h(naive.NButton, { type: 'info' }, '新增'),
+            h(naive.NButton, { type: 'info', onClick: handleAddAuth }, '新增'),
           ]),
           h(naive.NDataTable, {
             columns: columns,
@@ -90,6 +102,16 @@ export default {
             data: tableData.value,
           }),
         ]),
+
+        // 新增授权 dialog
+        h(
+          naive.NMessageProvider,
+          h(AddDialog, {
+            visible: adModalVisible.value,
+            'onUpdate:visible': handleAddModalCancel,
+            onClose: handleAddModalCancel,
+          })
+        ),
       ]);
   },
 };
