9d129aa74feac7322dd07eff04cb598846449e0b
singcl <1276837689@qq.com>
2022-06-18 09:51:44 +0800

    feat(fed): use naive ui data table

--- public/static/js/authorized/authorized_list.js
+++ public/static/js/authorized/authorized_list.js
@@ -3,6 +3,19 @@ import naive from '/public/static/js/vue/naive.js';
 // 绝对路径导入无法识别类型。 不知道怎么配置？
 import Kiko from './../utils/kiko/Kiko.js';
 
+const useOptions = [
+  {
+    value: 1,
+    label: '启用',
+    type: 'success',
+  },
+  {
+    value: -1,
+    label: '禁用',
+    type: 'warning',
+  },
+];
+
 //
 const defaultColumns = [
   { key: 'id', title: '编号' },
@@ -10,8 +23,29 @@ const defaultColumns = [
   { key: 'business_developer', title: '对接人' },
   { key: 'created_at', title: '创建日期' },
   { key: 'updated_at', title: '更新日期' },
-  { key: 'is_used', title: '状态' },
-  { key: 'operation', title: '操作' },
+  {
+    key: 'is_used',
+    title: '状态',
+    render(row) {
+      const { is_used } = row;
+      const itm = useOptions.find((m) => m.value == is_used);
+      return itm ? h(naive.NTag, { type: itm.type }, itm.label) : is_used;
+    },
+  },
+  {
+    key: 'operation',
+    title: '操作',
+    render(row) {
+      const { is_used } = row;
+      const op_used = { 1: -1, '-1': 1 }[is_used];
+      const itm = useOptions.find((m) => m.value == op_used);
+      return h('div', [
+        h(naive.NButton, { type: 'info', style: { marginRight: '10px' } }, '详情'),
+        itm ? h(naive.NButton, { type: itm.type, style: { marginRight: '10px' } }, itm.label) : undefined,
+        h(naive.NButton, { type: 'error' }, '删除'),
+      ]);
+    },
+  },
 ];
 
 // App
