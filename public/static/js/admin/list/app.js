4eb6710576e41bdfb673de3ed9cc6eb3bc9d5453
singcl <1276837689@qq.com>
2022-06-19 13:47:15 +0800

    feat(fed): page for query admin users list

---
+++ public/static/js/admin/list/app.js
@@ -0,0 +1,10 @@
+import { h } from '/public/static/js/vue/vue3.esm-browser.js';
+import naive from '/public/static/js/vue/naive.js';
+import Page from './admin_list.js';
+const { NMessageProvider, NDialogProvider } = naive;
+
+export default {
+  setup() {
+    return () => h(NMessageProvider, () => h(NDialogProvider, () => h(Page)));
+  },
+};
