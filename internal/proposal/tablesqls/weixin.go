495923bf444083fc5b1dc6124a2256009d7335f7
singcl <1276837689@qq.com>
2022-06-23 13:53:56 +0800

    feat(wx): wx table sql update column

--- internal/proposal/tablesqls/weixin.go
+++ internal/proposal/tablesqls/weixin.go
@@ -1,8 +1,10 @@
 package tablesqls
 
-// CREATE TABLE `wx_user` (
+// CREATE TABLE `weixin` (
 // `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
 // `openid` varchar(60) NOT NULL DEFAULT '' COMMENT '微信openid',
+// `unionid` varchar(60) NOT NULL DEFAULT '' COMMENT '微信unionid',
+// `session_key` varchar(60) NOT NULL DEFAULT '' COMMENT '微信session_key',
 // `username` varchar(32) NOT NULL DEFAULT '' COMMENT '微信用户名',
 // `nickname` varchar(60) NOT NULL DEFAULT '' COMMENT '昵称',
 // `mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '手机号',
@@ -17,9 +19,11 @@ package tablesqls
 // ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='微信小程序登录用户表';
 
 func CreateWxUserTableSql() (sql string) {
-	sql = "CREATE TABLE `wx_user` ("
+	sql = "CREATE TABLE `weixin` ("
 	sql += "`id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',"
 	sql += "`openid` varchar(60) NOT NULL DEFAULT '' COMMENT '微信openid',"
+	sql += "`unionid` varchar(60) NOT NULL DEFAULT '' COMMENT '微信unionid',"
+	sql += "`session_key` varchar(60) NOT NULL DEFAULT '' COMMENT '微信session_key',"
 	sql += "`username` varchar(32) NOT NULL DEFAULT '' COMMENT '微信用户名',"
 	sql += "`nickname` varchar(60) NOT NULL DEFAULT '' COMMENT '昵称',"
 	sql += "`mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '手机号',"
