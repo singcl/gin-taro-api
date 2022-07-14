6db5095e527e3f9170ca376675e56f9d3e864cac
singcl <1276837689@qq.com>
2022-07-15 00:59:49 +0800

    refactor(api): delete error response

--- internal/services/authorized/service_delete.go
+++ internal/services/authorized/service_delete.go
@@ -6,7 +6,6 @@ import (
 	"github.com/singcl/gin-taro-api/internal/repository/mysql"
 	"github.com/singcl/gin-taro-api/internal/repository/mysql/authorized"
 	"github.com/singcl/gin-taro-api/internal/repository/redis"
-	"gorm.io/gorm"
 )
 
 func (s *service) Delete(ctx core.Context, id int32) (err error) {
@@ -16,8 +15,11 @@ func (s *service) Delete(ctx core.Context, id int32) (err error) {
 		WhereId(mysql.EqualPredicate, id).
 		First(s.db.GetDbR().WithContext(ctx.RequestContext()))
 
-	if err == gorm.ErrRecordNotFound {
-		return nil
+		// if err == gorm.ErrRecordNotFound {
+		// 	return nil
+		// }
+	if err != nil {
+		return err
 	}
 
 	data := map[string]interface{}{
