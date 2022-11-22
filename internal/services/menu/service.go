331674e25ce9ebf0644c736aa078d843a57f9302
singcl <1276837689@qq.com>
2022-11-22 17:45:23 +0800

    feat(api): menu list api service

--- internal/services/menu/service.go
+++ internal/services/menu/service.go
@@ -3,6 +3,7 @@ package menu
 import (
 	"github.com/singcl/gin-taro-api/internal/pkg/core"
 	"github.com/singcl/gin-taro-api/internal/repository/mysql"
+	"github.com/singcl/gin-taro-api/internal/repository/mysql/menu"
 	"github.com/singcl/gin-taro-api/internal/repository/redis"
 )
 
@@ -13,6 +14,7 @@ type Service interface {
 
 	Create(ctx core.Context, menuData *CreateMenuData) (id int32, err error)
 	Modify(ctx core.Context, id int32, menuData *UpdateMenuData) (err error)
+	List(ctx core.Context, searchData *SearchData) (listData []*menu.Menu, err error)
 }
 
 type service struct {
