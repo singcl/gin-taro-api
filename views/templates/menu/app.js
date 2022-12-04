b8bfc4d83335029c21435c12f8d639eb2bd9cb66
singcl <1276837689@qq.com>
2022-12-04 23:30:56 +0800

    feat(html): menu html moving

--- views/templates/menu/app.js
+++ views/templates/menu/app.js
@@ -1,5 +1,5 @@
-import { h } from '/public/static/js/vue/vue3.esm-browser.js';
-import naive from '/public/static/js/vue/naive.js';
+import { h } from '/views/static/js/vue/vue3.esm-browser.js';
+import naive from '/views/static/js/vue/naive.js';
 import Page from './menu_view.js';
 const { NMessageProvider, NDialogProvider } = naive;
 
--- views/templates/menu/menu_view.js
+++ views/templates/menu/menu_view.js
@@ -1,4 +1,4 @@
-import { h /* reactive, ref, onMounted */ } from '/public/static/js/vue/vue3.esm-browser.js';
+import { h /* reactive, ref, onMounted */ } from '/views/static/js/vue/vue3.esm-browser.js';
 // import naive from '/public/static/js/vue/naive.js';
 // 绝对路径导入无法识别类型。 不知道怎么配置？
 // import Kiko from './../utils/kiko/Kiko.js';
