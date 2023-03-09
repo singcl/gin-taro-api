b1bbbc700c5b8709d2f39c8878e509bddd845dea
singcl <1276837689@qq.com>
2023-03-10 00:38:59 +0800

    chore(build): min.js

--- views/templates/admin/admin_login/app.js
+++ views/templates/admin/admin_login/app.js
@@ -1,5 +1,7 @@
-import { h } from '/views/static/js/vue/vue3.esm-browser.js';
-import naive from '/views/static/js/vue/naive.js';
+// import { h } from '/views/static/js/vue/vue3.esm-browser.min.js';
+import { h } from 'vue';
+// import naive from '/views/static/js/vue/naive.min.js';
+import naive from 'naive';
 import Page from './admin_login.js';
 const { NMessageProvider, NDialogProvider, NNotificationProvider } = naive;
 
