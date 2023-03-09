2190cded4f6e0563439bb9d68ef4a14fe502d2d6
singcl <1276837689@qq.com>
2023-03-10 00:26:11 +0800

    chore(build): gulp minify

--- views/static/js/vue/naive.js
+++ views/static/js/vue/naive.js
@@ -1,5 +1,5 @@
-import * as vue from './vue3.esm-browser.js'
-import Naive from "./naive-ui-es-browser.js";
+import * as vue from './vue3.esm-browser.min.js'
+import Naive from "./naive-ui-es-browser.min.js";
 
 const naive = {};
 Naive(naive, vue);
