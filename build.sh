ca7069d2ca74681f5432db5755bdc0f47c8d3bbc
singcl <1276837689@qq.com>
2023-03-09 15:08:15 +0800

    chore(ci): build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
     echo "[go pid not found]"
 else
 	#杀掉进程
-    kill -9 $go_id
+    sudo kill -9 $go_id
     echo "killed $go_id"
 fi
 
@@ -16,4 +16,13 @@ echo "clean old file"
 rm -rf ${project_name}
 #执行日志，根据自己项目情况可选
 rm -rf ${project_name}.log
-
+if [ -f main ]; then
+    echo "strat new process"
+    # workspace中运行..
+    mv main ${project_name}
+    sudo chmod -R 777 ${project_name}
+    #这里要防止nohup不执行，添加了一个BUILD_ID
+    BUILD_ID=DONTKILLME nohup ./${project_name} >${project_name}.log 2>&1 &
+else
+echo "executable file not found,quit"
+fi
